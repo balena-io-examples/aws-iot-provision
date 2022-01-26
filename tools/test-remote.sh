1fbe43129c33c538814051f43f6d0628913668f0
Ken Bannister <kb2ma@runbox.com>
2022-01-26 10:04:39 -0500

    Refine comments for test file.
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- tools/test-remote.sh
+++ tools/test-remote.sh
@@ -4,8 +4,12 @@
 #    * HTTP method -- POST or DELETE
 #
 #    test-remote.sh <POST|DELETE>
+#
+# Usage:
+#    * Use your values for variables of the form "<your-*>" in this file.
 
 BALENA_DEVICE_UUID=<your-uuid>
+# From AWS API Gateway endpoint
 PROVISION_URL=<your-url>
 
 curl -X POST $PROVISION_URL -H "Content-Type:application/json" \
