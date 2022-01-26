e2ba16acb06bac40e0a7bd82ced094de166cc6a1
Ken Bannister <kb2ma@runbox.com>
2022-01-26 09:07:01 -0500

    Minor comment fix
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- tools/test-local.sh
+++ tools/test-local.sh
@@ -10,7 +10,7 @@
 #    * Copy this file, run.env, and the repository /src directory to the
 #      working directory.
 #    * Use your values for variables of the form "<your-*>" in this file and
-#      deploy.env.
+#      run.env.
 #    * In the working directory, run 'npm install'.
 #    * Then run this file, including the method parameter as shown above.
 
